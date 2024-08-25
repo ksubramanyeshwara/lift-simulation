475640521fb76b46b462724e46cbd7a55059a1b1
K Subramanyeshwara <subramanyeshwarak@gmail.com>
2024-08-25 18:55:17 +0530

    dynamic floor and lift creation upon submitting the form

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+const liftForm = document.querySelector("#lift-form");
+liftForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const numFloors = parseInt(document.getElementById("floor").value, 10);
+  const numLifts = parseInt(document.getElementById("lift").value, 10);
+  const building = document.getElementById("building");
+  liftForm.style.display = "none";
+
+  for (let i = numFloors; i > 0; i--) {
+    const floorContainer = document.createElement("div");
+    floorContainer.className = "floorContainer";
+
+    const btnContainer = document.createElement("div");
+    btnContainer.id = "btnContainer";
+    btnContainer.innerHTML = `<p>Floor - ${i}</p>`;
+    if (i < numFloors) {
+      btnContainer.innerHTML += '<button type="button" class="up">Up</button>';
+    }
+    if (i > 1) {
+      btnContainer.innerHTML +=
+        '<button type="button" class="down">Down</button>';
+    }
+
+    const liftContainer = document.createElement("div");
+    liftContainer.id = "liftContainer";
+
+    floorContainer.appendChild(btnContainer);
+
+    for (let j = 0; j < numLifts; j++) {
+      const liftContainer = document.createElement("div");
+      liftContainer.id = `liftContainer-${i}-${j}`; // Unique ID for each lift on each floor
+      liftContainer.className = "liftContainer"; // Use class for similar elements
+      // liftContainer.style.width = "20px";
+      // liftContainer.style.height = "20px";
+      // liftContainer.style.border = "1px solid black";
+      // liftContainer.style.display = "inline-block";
+      // liftContainer.style.margin = "5px";
+      floorContainer.appendChild(liftContainer);
+    }
+
+    building.appendChild(floorContainer);
+  }
+});
